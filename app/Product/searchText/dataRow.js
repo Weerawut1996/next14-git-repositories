164f1e29c4ad4d652c78a72f4d9b6282721cd85e
Weerawut1996 <wirawoot789@hotmail.com>
2024-03-08 16:17:03 +0700

    add dataRow

---
+++ app/Product/searchText/dataRow.js
@@ -0,0 +1,38 @@
+
+{/* JavaScript สำหรับค้นหาและแสดงผลข้อมูล */ }
+ const dataRow =[
+   {
+      ID: 1,
+      person_id: "12345",
+      Name: "John",
+      card_id: "67890",
+      b_date: "01/01/1990",
+      telephone: "555-5555"
+    },
+    {
+      ID: 2,
+      person_id: "aaa12345",
+      Name: "Json",
+      card_id: "678901515",
+      b_date: "05/09/1996",
+      telephone: "444-5555"
+    },
+    {
+      ID: 3,
+      person_id: "bbbb1234",
+      Name: "Jaba",
+      card_id: "123451515",
+      b_date: "05/09/1996",
+      telephone: "555-6666"
+    },
+    {
+      ID: 4,
+      person_id: "dddd12345",
+      Name: "jj",
+      card_id: "678901515",
+      b_date: "05/09/1996",
+      telephone: "555-6666"
+    }
+]
+
+export default dataRow;
