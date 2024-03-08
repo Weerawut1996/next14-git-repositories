f7ac14ac9f95dcbea7f3ad0bdcb27a9d8d056ca2
Weerawut1996 <wirawoot789@hotmail.com>
2024-03-08 16:18:33 +0700

    Edit display

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,15 @@ const ver2 = version;
 
 
 export default function Home() {
-  
+
    return (
       <main>
-         <p>React.version : {ver}</p>
-         <p>version       : {ver2}</p>
+         <div className="inline-grid grid-cols-[auto_auto] gap-x-2">
+            <p className="text-center">React.version</p><p>: {ver}</p>
+            <p className="text-center">version</p><p>: {ver2}</p>
+         </div>
          <h1 className="text-4xl font-bold text-center">Home</h1>
-         
+
          <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert text-center"
             src="/next.svg"
