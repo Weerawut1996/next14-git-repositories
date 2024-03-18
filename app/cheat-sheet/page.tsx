a5c39943b116422a06049e2fdecd8b6bb9e4d625
Weerawut1996 <wirawoot789@hotmail.com>
2024-03-18 16:34:40 +0700

    remove Cheat-sheet

--- app/cheat-sheet/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function CheatSheet() {
-   return (
-         <main>
-            <h1 className="text-4xl font-bold text-center">Cheat Sheet</h1>            
-         </main>
-   );
-}
