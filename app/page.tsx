ebb97e1948f6838f03d5d5fee899fa8cf1e077b8
Weerawut1996 <wirawoot789@hotmail.com>
2024-04-01 16:39:31 +0700

    move chack version

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
 import Image from "next/image";
-import React from "react";
-
-
-import { version } from 'react';
-const ver = React.version;
-const ver2 = version;
-
-
+import _s from "@/app/_G.module.css"
 
 export default function Home() {
 
    return (
-      <main>
-         <div className="inline-grid grid-cols-[auto_auto] gap-x-2">
-            <p className="text-center">React.version</p><p>: {ver}</p>
-            <p className="text-center">version</p><p>: {ver2}</p>
-         </div>
+      <main className={_s.item3}>
          <h1 className="text-4xl font-bold text-center">Home</h1>
-
          <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert text-center"
             src="/next.svg"
