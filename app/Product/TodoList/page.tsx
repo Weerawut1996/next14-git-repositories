f2f4e4f051b9c6a4b80866896940dbcd95a07e73
Weerawut1996 <wirawoot789@hotmail.com>
2024-05-01 16:36:24 +0700

    add todo page

---
+++ app/Product/TodoList/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+
+export default function TodoListPage() {
+   
+   return(
+      <section>
+      <h1 className="text-4xl font-bold text-center">Todo List</h1>
+      </section>
+   )
+}
